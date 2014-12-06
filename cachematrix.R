cb37b9e84144ccd032171a92ab9b81069f801d54
edamiee <damien.k.edwards@gmail.com>
2014-12-06 15:49:19 -0600

    Changes 12/6/2014c

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,22 @@
+##Damien Edwards
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 
 }
 
